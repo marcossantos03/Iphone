529536aa0c2d6522377837b1cd881d0a0b8e9ea7
marcossantos03 <marcospalmeirense@live.com>
2024-01-03 20:31:54 -0300

    iphone

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+function trocacor(cor){
+    let circulo = document.querySelector(".circulo")
+circulo.style.background = cor
+}
+
+function trocaimagem(imagem){
+    let imgiphone = document.querySelector(".iphone")
+    imgiphone.src = imagem
+} 
